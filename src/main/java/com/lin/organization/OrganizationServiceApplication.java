12114bd84f7b80a35d34c45a54b2ab63034df276
ChenLin12138 <scorpion_chenlin@163.com>
2019-09-14 21:34:46 +0800

    添加处理关联id逻辑

--- src/main/java/com/lin/organization/OrganizationServiceApplication.java
+++ src/main/java/com/lin/organization/OrganizationServiceApplication.java
@@ -2,10 +2,12 @@ package com.lin.organization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ServletComponentScan(basePackages = "com.lin.organization.*")
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
