9dd5d08f338bc010a911e99ce55ab75f9f05aefa
ChenLin12138 <scorpion_chenlin@163.com>
2019-10-10 21:54:50 +0800

    添加数据

--- createTable.sql
+++ createTable.sql
@@ -6,3 +6,7 @@ CREATE TABLE `organizations` (
   `contact_phone` varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`organization_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+INSERT INTO organization.organizations
+(organization_id, name, contact_name, contact_email, contact_phone)
+VALUES('35690f33-71b1-4996-83f8-93b8ca411848', 'orgnam', 'testorgName', 'scorpion_chenlin@163.com', '13499098898');
